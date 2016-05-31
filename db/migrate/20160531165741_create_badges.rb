eecdd0bdeda880443ec956e20613c6cd160e58b5
Renan Martins & David Tao <pair+nbkhope.taodav@devbootcamp.com>
2016-05-31 12:18:55 -0700

    Add student foreign key to badges table in migration

--- db/migrate/20160531165741_create_badges.rb
+++ db/migrate/20160531165741_create_badges.rb
@@ -2,6 +2,7 @@ class CreateBadges < ActiveRecord::Migration
   def change
     create_table :badges do |t|
       t.string :content
+      t.references :student, index: true, foreign_key: true
 
       t.timestamps null: false
     end
