06a2f0b332aed028c8ac3f215fbb9e13ce1fbaf9
Renan Martins & David Tao <pair+nbkhope.taodav@devbootcamp.com>
2016-05-31 15:27:11 -0700

    Add voting routes for badge controller

--- app/controllers/badges_controller.rb
+++ app/controllers/badges_controller.rb
@@ -1,6 +1,14 @@
 class BadgesController < ApplicationController
-  def index
+  def vote
+    badge = Badge.find(params[:id])
+    badge.votes.create(vote_params)
+  end
 
+  private
+  def vote_params
+    return params.require(:vote).permit(:vote_type, :user)
   end
 
 end
+
+#
