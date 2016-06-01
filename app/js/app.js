c585d4605e92a016d00aa768509577b68f196093
Renan Martins & David Tao <pair+nbkhope.taodav@devbootcamp.com>
2016-05-31 17:41:54 -0700

    Add jquery for clicking on student link

--- app/js/app.js
+++ app/js/app.js
@@ -13,14 +13,44 @@ $(document).ready(function() {
   var context = {};
 
   request.done(function(response) {
-    console.log(response);
+    console.log()
+    // context.students = JSON.parse(response);
     context.students = response;
-    console.log(context);
     // Pass our data to the template
     var theCompiledHtml = theTemplate(context);
 
     // Add the compiled html to the page
     $('.content-placeholder').html(theCompiledHtml);
+  });
+
+  // Fix this for miniQuery
+  $('body').on('click', 'a', function(e) {
+    e.preventDefault();
+    console.log("I've been clicked");
+
+    $.ajax({
+      url: "http://localhost:3000/students/" + $(this).attr('id'),
+      method: "GET"
+    }).done(function(response) {
+      console.log(response);
+      // var res = JSON.parse(response);
+      var res = response;
+      context.badges = res.badges;
+
+      var badgesList = document.createElement("div");
+      for (badge of res.badges) {
+        var paragraph = document.createElement("p");
+        var badgeContent = document.createTextNode(badge.content);
+        paragraph.appendChild(badgeContent);
+        console.log(paragraph);
+        badgesList.appendChild(paragraph);
+      }
+
+      document.getElementById("student-" + res.id).appendChild(badgesList);
+      // $('div#student-' + res.id).
+
+    });
+
   });
 
 });
